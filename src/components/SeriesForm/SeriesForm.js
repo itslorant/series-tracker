49823c66e37b90ca34701561b18564d5c4342ba5
UsmashD <fihumoto@gmail.com>
2022-02-25 19:53:34 +0100

    added mui elements

--- src/components/SeriesForm/SeriesForm.js
+++ src/components/SeriesForm/SeriesForm.js
@@ -1,20 +1,30 @@
 import React, { useState } from "react";
 
 // import InputField from "../UI/Input/Input";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
 export default function SeriesForm(props) {
   const [title, setTitle] = useState("");
   const [season, setSeason] = useState("");
   const [episode, setEpisode] = useState("");
+  const [extraInfo, setExtraInfo] = useState("");
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onAddSeries({title: title, season: season, episode: episode});
+    props.onSubmit({
+      title: title,
+      season: season,
+      episode: episode,
+      extraInfo: extraInfo,
+    });
   };
 
   return (
     <React.Fragment>
       <form onSubmit={submitHandler}>
-        <input
+        <TextField
+          label="text"
           type="text"
           id="title"
           name="title"
@@ -22,8 +32,10 @@ export default function SeriesForm(props) {
           onChange={(event) => {
             setTitle(event.target.value);
           }}
+          size="small"
         />
-        <input
+        <TextField
+          label="season"
           type="text"
           id="season"
           name="season"
@@ -31,8 +43,10 @@ export default function SeriesForm(props) {
           onChange={(event) => {
             setSeason(event.target.value);
           }}
+          size="small"
         />
-        <input
+        <TextField
+          label="episode"
           type="text"
           id="episode"
           name="episode"
@@ -40,8 +54,20 @@ export default function SeriesForm(props) {
           onChange={(event) => {
             setEpisode(event.target.value);
           }}
+          size="small"
         />
-        <button type="submit">Submit</button>
+        <TextField
+          label="extraInfo"
+          type="text"
+          id="extraInfo"
+          name="extraInfo"
+          value={extraInfo}
+          onChange={(event) => {
+            setExtraInfo(event.target.value);
+          }}
+          size="small"
+        />
+        <Button variant="contained" type="submit">Submit</Button>
       </form>
     </React.Fragment>
   );
