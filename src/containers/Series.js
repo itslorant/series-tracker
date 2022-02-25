4486ada041748a6cad2fcf31677eea6cc54075f5
UsmashD <fihumoto@gmail.com>
2022-02-25 19:54:00 +0100

    EditSeries comp added

--- src/containers/Series.js
+++ src/containers/Series.js
@@ -3,6 +3,7 @@ import React, { useEffect, useReducer, useState } from "react";
 import axios from "axios";
 
 import SeriesForm from "../components/SeriesForm/SeriesForm";
+import EditSeries from "../components/EditSeries/EditSeries";
 import SeriesList from "../components/SeriesList/SeriesList";
 
 const seriesReducer = (currentSeries, action) => {
@@ -22,10 +23,11 @@ export default function Series() {
   const [userSeries, dispatch] = useReducer(seriesReducer, []);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
+  const [seriesId, setSeriesId] = useState(null);
 
   useEffect(() => {
     axios({
-      url: process.env.REACT_APP_BASE_URL+"/series.json",
+      url: process.env.REACT_APP_BASE_URL + "/series.json",
       method: "GET",
     })
       .then((response) => {
@@ -36,6 +38,7 @@ export default function Series() {
             title: response.data[key].title,
             season: response.data[key].season,
             episode: response.data[key].episode,
+            extraInfo: response.data[key].extraInfo,
           });
         }
         dispatch({ type: "SET", series: series });
@@ -43,9 +46,9 @@ export default function Series() {
       .catch((error) => setError(error.message));
   }, [isLoading]);
 
-  const addSeriesHandler = (series) => {
+  const onAddSeriesHandler = (series) => {
     axios({
-      url: process.env.REACT_APP_BASE_URL+"/series.json",
+      url: process.env.REACT_APP_BASE_URL + "/series.json",
       method: "POST",
       data: series,
     })
@@ -58,12 +61,58 @@ export default function Series() {
       })
       .catch((error) => setError(error.message));
   };
+
+  const onDeleteSeriesHandler = (event) => {
+    const seriesId = event.target.parentNode.id;
+    axios({
+      url: process.env.REACT_APP_BASE_URL + `/series/${seriesId}.json`,
+      method: "DELETE",
+    })
+      .then((response) => {
+        dispatch({
+          type: "DELETE",
+          serie: { id: seriesId },
+        });
+        setIsLoading((prevLoading) => !prevLoading);
+      })
+      .catch((error) => setError(error.message));
+  };
+
+  const onModifySeriesHandler = (event) => {
+    const seriesId = event.target.parentNode.id;
+    setSeriesId(seriesId)
+  };
+
+  const modifySeries = (series)=>{
+    axios({
+      url: process.env.REACT_APP_BASE_URL + `/series/${seriesId}.json`,
+      method: "PATCH",
+      data: series,
+    })
+      .then((response) => {
+        setIsLoading((prevLoading) => !prevLoading);
+      })
+      .catch((error) => setError(error.message));
+      setSeriesId(null)
+  }
+
+
+  let seriesForm = <SeriesForm onSubmit={onAddSeriesHandler} />;
+  if (seriesId) {
+    seriesForm = <EditSeries onSubmit={modifySeries} seriesData={userSeries.filter((se) => se.id === seriesId)[0]} />;
+  }
+  
+  
   return (
     <React.Fragment>
       {error}
-      {console.log("rendered")}
-      <SeriesForm onAddSeries={addSeriesHandler} />
-      <SeriesList series={userSeries} />
+      {console.log("rendered", seriesId)}
+      {seriesForm}
+      <SeriesList
+        series={userSeries}
+        onModifySeries={(event) => onModifySeriesHandler(event)}
+        onDeleteSeries={(event) => onDeleteSeriesHandler(event)}
+      />
     </React.Fragment>
   );
 }
