355bbbc5e18ac88d97e0b120d7ebed7efd17dfec
UsmashD <fihumoto@gmail.com>
2022-03-29 22:50:00 +0200

    table header fixed, list sorted alphabetically

--- src/components/SeriesList/SeriesList.js
+++ src/components/SeriesList/SeriesList.js
@@ -7,63 +7,69 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 import style from "./SeriesList.module.css";
 
 export default function SeriesList(props) {
-  const list = props.series.map((se) => {
-    return {
-      id: se.id,
-      title: se.title,
-      season: se.season,
-      episode: se.episode,
-      extra: se.extraInfo,
-    };
-  });
+  const list = props.series
+    .sort((a, b) => {
+      return a.title > b.title;
+    })
+    .map((se) => {
+      return {
+        id: se.id,
+        title: se.title,
+        season: se.season,
+        episode: se.episode,
+        extra: se.extraInfo,
+      };
+    });
 
   return (
     <div className={style.Table}>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 350 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Title</TableCell>
-              <TableCell align="right">Season</TableCell>
-              <TableCell align="right">Episode</TableCell>
-              <TableCell align="right">Extra info</TableCell>
-              <TableCell align="right">Modify</TableCell>
-              <TableCell align="right">Delete</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {list.map((row) => (
-              <TableRow
-                key={row.id}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell>{row.title}</TableCell>
-                <TableCell align="right">{row.season}</TableCell>
-                <TableCell align="right">{row.episode}</TableCell>
-                <TableCell align="right">{row.extra}</TableCell>
-                <TableCell
-                  align="right"
-                  onClick={(e) => props.onModifySeries(e, row.id)}
-                >
-                  <EditIcon color="primary"/>
-                </TableCell>
-                <TableCell
-                  align="right"
-                  onClick={(e) => props.onDeleteSeries(e, row.id)}
-                >
-                  <DeleteIcon />
-                </TableCell>
+      <Paper sx={{ width: "100%", overflow: "hidden" }}>
+        <TableContainer sx={{ maxHeight: 840 }}>
+          <Table stickyHeader aria-label="sticky table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Title</TableCell>
+                <TableCell align="right">Season</TableCell>
+                <TableCell align="right">Episode</TableCell>
+                <TableCell align="right">Extra info</TableCell>
+                <TableCell align="right">Modify</TableCell>
+                <TableCell align="right">Delete</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+            </TableHead>
+            <TableBody>
+              {list.map((row) => (
+                <TableRow
+                  key={row.id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell>{row.title}</TableCell>
+                  <TableCell align="right">{row.season}</TableCell>
+                  <TableCell align="right">{row.episode}</TableCell>
+                  <TableCell align="right">{row.extra}</TableCell>
+                  <TableCell
+                    align="right"
+                    onClick={(e) => props.onModifySeries(e, row.id)}
+                  >
+                    <EditIcon color="primary" />
+                  </TableCell>
+                  <TableCell
+                    align="right"
+                    onClick={(e) => props.onDeleteSeries(e, row.id)}
+                  >
+                    <DeleteIcon />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
     </div>
   );
 }
