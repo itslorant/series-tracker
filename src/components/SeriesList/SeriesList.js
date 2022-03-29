4cd9edfa9fa7510dce4163ba8e5b3c8bf14455b3
UsmashD <fihumoto@gmail.com>
2022-03-29 22:25:54 +0200

    added mui table instead of html table

--- src/components/SeriesList/SeriesList.js
+++ src/components/SeriesList/SeriesList.js
@@ -1,30 +1,69 @@
 import React from "react";
 
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+import style from "./SeriesList.module.css";
+
 export default function SeriesList(props) {
   const list = props.series.map((se) => {
-    return (
-      <tr key={se.id} id={se.id}>
-        <td>{se.title}</td>
-        <td>{se.season}</td>
-        <td>{se.episode}</td>
-        <td>{se.extraInfo}</td>
-        <td onClick={props.onModifySeries}>Modify</td>
-        <td onClick={props.onDeleteSeries}>Delete</td>
-      </tr>
-    );
+    return {
+      id: se.id,
+      title: se.title,
+      season: se.season,
+      episode: se.episode,
+      extra: se.extraInfo,
+    };
   });
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Title</th>
-          <th>Season</th>
-          <th>Episode</th>
-          <th>Extra info</th>
-        </tr>
-      </thead>
-      <tbody>{list}</tbody>
-    </table>
+    <div className={style.Table}>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 350 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Title</TableCell>
+              <TableCell align="right">Season</TableCell>
+              <TableCell align="right">Episode</TableCell>
+              <TableCell align="right">Extra info</TableCell>
+              <TableCell align="right">Modify</TableCell>
+              <TableCell align="right">Delete</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {list.map((row) => (
+              <TableRow
+                key={row.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell>{row.title}</TableCell>
+                <TableCell align="right">{row.season}</TableCell>
+                <TableCell align="right">{row.episode}</TableCell>
+                <TableCell align="right">{row.extra}</TableCell>
+                <TableCell
+                  align="right"
+                  onClick={(e) => props.onModifySeries(e, row.id)}
+                >
+                  <EditIcon color="primary"/>
+                </TableCell>
+                <TableCell
+                  align="right"
+                  onClick={(e) => props.onDeleteSeries(e, row.id)}
+                >
+                  <DeleteIcon />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
   );
 }
